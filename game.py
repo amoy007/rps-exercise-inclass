564ef975c7f150d682684ff5b8c8dc0a90d161d9
amoy007 <50883306+amoy007@users.noreply.github.com>
2019-06-05 18:56:40 -0400

    Capture inputs
    created user_choice variable and make it user friendly by printing out what the user chose.

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+
+# CAPTURE INPUTS
+# 1. Create variables
+
+user_choice = input("Please choose one of the following options 'rock', 'paper', or 'scissors' (without the quotes): ")
+print("-------------")
+print("You Chose: ", user_choice)
+
+# VALIDATE INPUTS
+# 
+# GENERATE COMPUTER SELECTION
+# 
+# DETERMINE THE WINNER
+# 
+# DISPLAY FINAL OUTPUTS / OUTCOMES
