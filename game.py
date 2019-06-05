4515c64ac66fb57699bdad447af0e8765fd519f2
amoy007 <50883306+amoy007@users.noreply.github.com>
2019-06-05 19:33:00 -0400

    one variable for choices
    created valid_options variable to be consistent across all statements.

--- game.py
+++ game.py
@@ -14,7 +14,9 @@ print("You Chose: ", user_choice)
 # VALIDATE INPUTS
 #  3. Create a conditional statement and list to validate.
 
-if user_choice not in ["rock", "paper", "scissors"]:
+valid_options = ["rock", "paper", "scissors"]
+
+if user_choice not in valid_options:
     print("INVALID SELECTION. PLEASE TRY AGAIN.")
     exit()
 
@@ -22,7 +24,7 @@ if user_choice not in ["rock", "paper", "scissors"]:
 # 
 print("GENERATING...")
 
-computer_choice = random.choice(["rock", "paper", "scissors"])
+computer_choice = random.choice(valid_options)
 
 print("--------------")
 print("GENERATING...")
